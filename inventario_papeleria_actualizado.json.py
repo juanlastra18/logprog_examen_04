f1e7bfe034e23f94da1b8eda86fe2b68f7c9d598
Juan Jose Lastra Hernandez <juan.lastra@upb.edu.co>
2025-05-15 12:50:38 -0500

    Create inventario_papeleria_actualizado.json.py

---
+++ inventario_papeleria_actualizado.json.py
@@ -0,0 +1,38 @@
+{
+    "UE001": {
+        "categoria": "utiles escolares",
+        "nombre": "cuaderno cuadriculado",
+        "cantidad": 56,
+        "precio": 3780
+    },
+    "UE002": {
+        "categoria": "utiles escolares",
+        "nombre": "lapiz 2B",
+        "cantidad": 134,
+        "precio": 864
+    },
+    "PO001": {
+        "categoria": "papeleria de oficina",
+        "nombre": "Resma Papel Carta",
+        "cantidad": 39,
+        "precio": 1296
+    },
+    "PO002": {
+        "categoria": "papeleria de oficina",
+        "nombre": "Carpeta Archivadora",
+        "cantidad": 28,
+        "precio": 4860
+    },
+    "MA001": {
+        "categoria": "Manualidades",
+        "nombre": "Cartulina de Colores",
+        "cantidad": 89,
+        "precio": 1296
+    },
+    "MA002": {
+        "categoria": "Manualidades",
+        "nombre": "Kit Pinturas Acuarela",
+        "cantidad": 16,
+        "precio": 23760
+    }
+}
